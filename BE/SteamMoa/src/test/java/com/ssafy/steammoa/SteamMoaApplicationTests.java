6c1a954a71f706e532590024794ddebb4ade43d7
김다은 <vkfksqlcgkxm@naver.com>
2022-07-21 13:13:59 +0900

    init BE - 프로젝트 기본 셋팅

---
+++ BE/SteamMoa/src/test/java/com/ssafy/steammoa/SteamMoaApplicationTests.java
@@ -0,0 +1,24 @@
+package com.ssafy.steammoa;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Slf4j
+class SteamMoaApplicationTests {
+
+    @Autowired
+//    UserService uService;
+
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void serviceTest(){
+//        Assert.notNull(uService);
+    }
+
+}
