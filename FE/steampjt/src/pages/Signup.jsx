aaf98881ea90adcd72dde55e98a58fdbc368b171
NamjuPark-dev <dev.nammjuicy@gmail.com>
2022-07-22 22:27:24 +0900

    fix:브랜치 분기 실수 수정

--- FE/steampjt/src/pages/Signup.jsx
+++ FE/steampjt/src/pages/Signup.jsx
@@ -1,27 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-import axios from "axios";
+import axios from 'axios';
 
 
 const Signup = (props) => {
 
   const handleSteamAuth = () => {
-      axios({
-          url: "https://steamcommunity.com/openid/login",
-          method: 'post',
-          data: {
-              'openid.identity': "http://specs.openid.net/auth/2.0/identifier_select",
-              'openid.claimed_id': "http://specs.openid.net/auth/2.0/identifier_select",
-              'openid.ns': "http://specs.openid.net/auth/2.0",
-              'openid.mode': "checkid_setup",
-              'openid.realm': "http://localhost:3000",
-              'openid.return_to': "http://localhost:3000"
-          }
-      }).then((res) => {
-        console.log(res);
-      });
-    // 중앙에 userSignupID 저장 후 회원가입 폼 날릴때 해당 정보 포함, userSignupID 초기화
+    
   }
+  
   return (
     <>
       <Navbar />
