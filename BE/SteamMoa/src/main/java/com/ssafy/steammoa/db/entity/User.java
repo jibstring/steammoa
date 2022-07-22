7a074750fcf5f21657d76d245ee22a29c2b23061
김다은 <vkfksqlcgkxm@naver.com>
2022-07-22 11:09:46 +0900

    feat : User 엔티티 작성 추후에 isAdmin을 Enum으로 변경할지 생각해봐야함

--- BE/SteamMoa/src/main/java/com/ssafy/steammoa/db/entity/User.java
+++ BE/SteamMoa/src/main/java/com/ssafy/steammoa/db/entity/User.java
@@ -36,14 +36,39 @@ public class User extends BaseEntity{
 //
 ////
 
+    // double유저식별자, 유저 스팀아이디()
+    @Id
+    @Column(name = "user_id")
+    @GeneratedValue
+    private Long id;
 
+    @Column(name = "user_steam_id", nullable = false)
+    private Long steamId;
 
-    String position;
-    String department;
-    String name;
-    String userId;
+    @Column(name = "user_service_id", nullable = false)
+    private Long userId;
 
     @JsonIgnore
-    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
-    String password;
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)  // 왜 쓰는지 -> https://eglowc.tistory.com/28
+    @Column(name = "user_service_pw")
+    private String password;
+
+    @Column(name = "user_name", nullable = false)
+    private String name;
+
+    // Enum 타입으로 바꿀지 고민중임.
+    @Column(name = "is_admin", nullable = false)
+    private boolean isAdmin;
+
+    @Column(name = "user_point")
+    private Double point;
+
+
+    @Column(name = "user_img_path")
+    private String imgPath;
+
+    @Column(name = "user_img_nm")
+    private String imgName;
+
+
 }
