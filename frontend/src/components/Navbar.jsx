44c046ff8c85a6aff7e27e179bef0b76282c4807
kimyeonjune <theresjahn@gmail.com>
2022-07-25 17:14:09 +0900

    feat: tailwind css 환경 설정 (tailwind.config.js)

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -8,24 +8,24 @@ const Navbar = (props) => {
     <div className="nav-container w-full nav-grad">
       {/* 내부 2칸 */}
       <div className="w-4/5 m-auto">
-        <div className="h-10 flex justify-between items-center">
+        <div className="h-9 flex justify-between items-center">
             {/* 로고 */}
-          <img src="../ImgAssets/TypoIconLogo.png" alt="Logo" className="h-3/6"/>
+          <img src="../ImgAssets/TypoIconLogo.png" alt="Logo" className="w-1/12"/>
           {/* 로그인 if 걸어야함 => 변수만들어서 IF문 */}
           <div className="flex items-end h-3/5">
-            <Link to="/" className="text-white text-sm">회원가입</Link>
-            <Link to="/login" className="text-white text-sm">로그인</Link>
+            <Link to="/" className="text-white text-xs font-sans mr-3 font-bold">회원가입</Link>
+            <Link to="/login" className="text-white text-xs font-sans ml-3 font-bold">로그인</Link>
           </div>
         </div>
-        <div className="h-12 flex justify-between items-center">
+        <div className="h-10 flex justify-between items-center">
           <div>
-            <Link to="/" className="text-white text-lg">홈</Link>
-            <Link to="/" className="text-white text-lg">모아존</Link>
-            <Link to="/" className="text-white text-lg">게임 모아</Link>
+            <Link to="/" className="text-white text-base font-blackSans mx-4">홈</Link>
+            <Link to="/" className="text-white text-base font-blackSans mx-4">모아존</Link>
+            <Link to="/" className="text-white text-base font-blackSans mx-4">게임 모아</Link>
           </div>
           <div>
               {/* 검색바 */}
-              <input type="text" id="small-input" className="block p-2 w-full text-gray-900 bg-gray-400 rounded-lg border border-gray-500 sm:text-xs focus:ring-slate-500 focus:border-slate-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
+              <input type="text" id="small-input" className="block p-1 w-full text-gray-900 bg-gray-400 rounded-lg border border-gray-500 sm:text-xs focus:ring-slate-500 focus:border-slate-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"></input>
 
           </div>
         </div>
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -8,6 +8,7 @@ module.exports = {
     extend: {
       fontFamily: {
         'sans': ['Noto Sans'],
+        'blackSans': ['Black Han Sans'],
       }
     },
   },
