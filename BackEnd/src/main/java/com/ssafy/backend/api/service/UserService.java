7311ce84d3221c7ab27bfc957498e1fa2af60151
김다은 <vkfksqlcgkxm@naver.com>
2022-07-25 18:03:57 +0900

    fix : userService 기능 추가

--- BackEnd/src/main/java/com/ssafy/backend/api/service/UserService.java
+++ BackEnd/src/main/java/com/ssafy/backend/api/service/UserService.java
@@ -1,12 +1,22 @@
 package com.ssafy.backend.api.service;
 
 import com.ssafy.backend.api.request.UserRegisterPostReq;
+import com.ssafy.backend.db.entity.Follow;
 import com.ssafy.backend.db.entity.User;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  *	유저 관련 비즈니스 로직 처리를 위한 서비스 인터페이스 정의.
  */
 public interface UserService {
-    User createUser(UserRegisterPostReq userRegisterInfo);
-    User getUserByUserId(String userId);
+    boolean createUser(UserRegisterPostReq userRegisterInfo);
+
+    Map<String, Object> getUserByUserId(String userId);
+    List<Follow> getFollowByUserId(Long userId);
+
+    // 유효성 검사
+    boolean checkSteamIdDuplicate(String steamId);
+    boolean checkServiceIdDuplicate(String serviceId);
 }
