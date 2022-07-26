66c9e6786fb86de7fd4f16ae187013f1de41d217
이성진 <leecjs000@naver.com>
2022-07-27 01:10:15 +0900

    feat : PtagStorage 엔티티 추가

--- BackEnd/src/main/java/com/ssafy/backend/db/entity/party/PtagStorage.java
+++ BackEnd/src/main/java/com/ssafy/backend/db/entity/party/PtagStorage.java
@@ -1,4 +1,13 @@
 package com.ssafy.backend.db.entity.party;
 
+import javax.persistence.*;
+
+@Entity
 public class PtagStorage {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long ptag_id;
+
+    private String ptag_content;
 }
