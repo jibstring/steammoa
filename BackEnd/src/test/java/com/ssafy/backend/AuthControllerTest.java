1938306786464f49d765e3b73b9d7b415da6d4d6
김다은 <vkfksqlcgkxm@naver.com>
2022-07-26 14:47:44 +0900

    init : 로그인 관련 테스트코드 파일 생성 AuthControllerTest

---
+++ BackEnd/src/test/java/com/ssafy/backend/AuthControllerTest.java
@@ -0,0 +1,33 @@
+package com.ssafy.backend;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ssafy.backend.api.controller.AuthController;
+import com.ssafy.backend.dto.UserDto;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+
+@SpringBootTest
+@Slf4j
+public class AuthControllerTest {
+
+    @Autowired
+    AuthController authController;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    @Test
+    public void requestBodyTest(){
+        UserDto userDto = new UserDto();
+        userDto.setUserServiceId("user_id");
+        userDto.setUserServicePw("user_pass");
+
+//        authController.login(userDto);
+
+    }
+}
