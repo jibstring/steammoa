ca5c07f4e30acbe613c9030f9fede0bfadca886f
이성진 <leecjs000@naver.com>
2022-07-27 01:03:38 +0900

    feat : PartyTag 엔티티 수정

--- BackEnd/src/main/java/com/ssafy/backend/db/entity/party/PartyTag.java
+++ BackEnd/src/main/java/com/ssafy/backend/db/entity/party/PartyTag.java
@@ -1,4 +1,22 @@
 package com.ssafy.backend.db.entity.party;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
 public class PartyTag {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
+    @JoinColumn(name="party_id")
+    private Party party;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
+    @JoinColumn(name="ptag_id")
+    private PtagStorage ptag;
 }
