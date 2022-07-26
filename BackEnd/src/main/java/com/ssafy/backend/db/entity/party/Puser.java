4ba88e9ec8ac793d82ddd924d764bd1771e0811b
이성진 <leecjs000@naver.com>
2022-07-27 00:49:23 +0900

    feat : Puser 엔티티 추가

--- BackEnd/src/main/java/com/ssafy/backend/db/entity/party/Puser.java
+++ BackEnd/src/main/java/com/ssafy/backend/db/entity/party/Puser.java
@@ -1,4 +1,24 @@
 package com.ssafy.backend.db.entity.party;
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.ssafy.backend.db.entity.User;
+import com.ssafy.backend.db.entity.game.Game;
 
+import javax.persistence.*;
+
+@Entity
 public class Puser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long party_id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
+    @JoinColumn(name="user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JsonBackReference
+    @JoinColumn(name="party_id")
+    private Party party;
 }
