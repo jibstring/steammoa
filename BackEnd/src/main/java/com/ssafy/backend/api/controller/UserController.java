4a87bc7f0a6519b1c872db4644cc9a30e96962d2
김다은 <vkfksqlcgkxm@naver.com>
2022-07-26 10:09:56 +0900

    fix : 설정 수정 properties 관련 수정

--- BackEnd/src/main/java/com/ssafy/backend/api/controller/UserController.java
+++ BackEnd/src/main/java/com/ssafy/backend/api/controller/UserController.java
@@ -1,16 +1,20 @@
 package com.ssafy.backend.api.controller;
 
 import com.ssafy.backend.api.request.UserRegisterPostReq;
-import com.ssafy.backend.api.response.UserRes;
 import com.ssafy.backend.api.service.UserService;
 import com.ssafy.backend.common.model.response.BaseResponseBody;
+import com.ssafy.backend.db.entity.Follow;
 import com.ssafy.backend.db.entity.User;
 import io.swagger.annotations.*;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.neo4j.Neo4jProperties;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import springfox.documentation.annotations.ApiIgnore;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 유저 관련 API 요청 처리를 위한 컨트롤러 정의.
@@ -18,7 +22,8 @@ import springfox.documentation.annotations.ApiIgnore;
  **/
 @Api(value = "유저 API", tags = {"User"})
 @RestController
-@RequestMapping("/api/v1/users")
+@RequestMapping("/api/users")
+@Slf4j
 public class UserController {
     @Autowired
     UserService userService;
@@ -30,27 +35,52 @@ public class UserController {
             @ApiResponse(code = 200, message = "성공"),
             @ApiResponse(code = 401, message = "인증 실패"),
             @ApiResponse(code = 404, message = "사용자 없음"),
+            @ApiResponse(code = 409, message = "아이디 중복 오류"),
             @ApiResponse(code = 500, message = "서버 오류")
     })
     public ResponseEntity<? extends BaseResponseBody> register(
             @RequestBody @ApiParam(value="회원가입 정보", required = true) UserRegisterPostReq registerInfo) {
 
         //임의로 리턴된 User 인스턴스. 현재 코드는 회원 가입 성공 여부만 판단하기 때문에 굳이 Insert 된 유저 정보를 응답하지 않음.
-        User user = userService.createUser(registerInfo);
-
+        boolean result = userService.createUser(registerInfo);
+        if(!result) return ResponseEntity.status(200).body(BaseResponseBody.of( 409, "Fail 아이디 중복"));
         return ResponseEntity.status(200).body(BaseResponseBody.of(200, "Success"));
     }
 
-//    public ResponseEntity<UserRes> getUserInfo(@ApiIgnore Neo4jProperties.Authentication authentication) {
-//        /**
-//         * 요청 헤더 액세스 토큰이 포함된 경우에만 실행되는 인증 처리이후, 리턴되는 인증 정보 객체(authentication) 통해서 요청한 유저 식별.
-//         * 액세스 토큰이 없이 요청하는 경우, 403 에러({"error": "Forbidden", "message": "Access Denied"}) 발생.
-//         */
+    @GetMapping("/{user_id}")
+    @ApiOperation(value = "회원 조회", notes = "<strong>사용자 아이디</strong>를 통해 회원 정보 조회.")
+    @ApiResponses({
+            @ApiResponse(code = 200, message = "성공"),
+            @ApiResponse(code = 401, message = "인증 실패"),
+            @ApiResponse(code = 404, message = "사용자 없음"),
+            @ApiResponse(code = 500, message = "서버 오류")
+    })
+
+    /* api/user/{user_id} , 노출되면 안됨
+    * api/user?user_id=123123 -> PathVariable , 노출 가능
+    * */
+    public ResponseEntity<?> getUserInfo(@RequestParam("user_service_id")String userId){
+        /**
+         * 요청 헤더 액세스 토큰이 포함된 경우에만 실행되는 인증 처리이후, 리턴되는 인증 정보 객체(authentication) 통해서 요청한 유저 식별.
+         * 액세스 토큰이 없이 요청하는 경우, 403 에러({"error": "Forbidden", "message": "Access Denied"}) 발생.
+         */
 //        SsafyUserDetails userDetails = (SsafyUserDetails)authentication.getDetails();
 //        String userId = userDetails.getUsername();
 //        User user = userService.getUserByUserId(userId);
-//
-//        return ResponseEntity.status(200).body(UserRes.of(user));
-//    }
+
+        // 생각해야할 부분 : body에 유저정보 + 파티정보 + 팔로워 정보 + 글 목록 받아와야함  / 이 데이터를 어떻게 묶어서 보낼것인지.
+        /*
+        * 1. Map<String, Object> 형식으로
+        * 2. 모든 Entity 의 toString을 재정의해라 (json방식)
+        * */
+        Map<String,Object> result = userService.getUserByUserId(userId);
+        log.debug("user 정보 : "+ result.toString());
+
+//        List<Follow> fList = userService.getFollowByUserId(userId);
+
+
+        return ResponseEntity.status(200).body(result);
+//        return ResponseEntity.status(200).body();
+    }
 
 }
--- BackEnd/src/main/java/com/ssafy/backend/api/response/UserRes.java
+++ BackEnd/src/main/java/com/ssafy/backend/api/response/UserRes.java
@@ -1,27 +1,38 @@
-package com.ssafy.backend.api.response;
-
-import com.ssafy.backend.db.entity.User;
-import com.ssafy.backend.common.model.response.BaseResponseBody;
-import com.ssafy.backend.db.entity.User;
-
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
-import lombok.Getter;
-import lombok.Setter;
-
-/**
- * 회원 본인 정보 조회 API ([GET] /api/v1/users/me) 요청에 대한 응답값 정의.
- */
-@Getter
-@Setter
-@ApiModel("UserResponse")
-public class UserRes{
-	@ApiModelProperty(name="User ID")
-	String userId;
-	
-	public static UserRes of(User user) {
-		UserRes res = new UserRes();
-		res.setUserId(user.getUserId());
-		return res;
-	}
-}
+//package com.ssafy.backend.api.response;
+//
+//import com.ssafy.backend.db.entity.Follow;
+//import com.ssafy.backend.db.entity.User;
+//import com.ssafy.backend.common.model.response.BaseResponseBody;
+//import com.ssafy.backend.db.entity.User;
+//
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiModel;
+//import io.swagger.annotations.ApiModelProperty;
+//import lombok.Data;
+//import lombok.Getter;
+//import lombok.Setter;
+//
+///**
+// * 회원 본인 정보 조회 API ([GET] /api/users/{user_id}) 요청에 대한 응답값 정의.
+// */
+////@Getter
+////@Setter
+//@Data
+//@ApiModel("UserResponse")
+//public class UserRes{
+//	@ApiModelProperty(name="User")
+//	User profile  = new User();
+//
+//	@ApiModelProperty(name="follow")
+//	Follow follow = new Follow();
+//
+////	@ApiModelProperty(name = "tacticList")
+//	// 게시글 리스트, 리뷰 리스트, 파티리스트
+//
+//	public UserRes(User user) {
+//		UserRes res = new UserRes(user);
+//
+//		res.setProfile(user);
+//		return res;
+//	}
+//}
