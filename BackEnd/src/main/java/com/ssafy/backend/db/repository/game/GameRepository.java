f33a4f68aaf8aec88fb2706564c95dfd92fb57d9
김다은 <vkfksqlcgkxm@naver.com>
2022-08-01 15:03:12 +0900

    fix : GameRepository 수정 사항 주석 추가

--- BackEnd/src/main/java/com/ssafy/backend/db/repository/game/GameRepository.java
+++ BackEnd/src/main/java/com/ssafy/backend/db/repository/game/GameRepository.java
@@ -13,6 +13,7 @@ import java.util.List;
 @Repository
 public interface GameRepository extends JpaRepository<Game, Long>, GameCustomRepository {
 
+    /* Optional<Game> 이런식으로 리턴타입 변경해주기~! ex. 가져올떄는 Optional받아온값.get()으로 받아오면 동일하게 동작합니다.*/
     Game findByGameId(Long gameId);
 
     // 멀티게임, 그리고 판매중(가격이 -1이 아닌)인 게임만 불러오도록 쿼리 수정 필요
