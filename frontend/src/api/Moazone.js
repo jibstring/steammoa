ef99096c3570da7cbb98ab5f0ff21c840b193344
NamjuPark-dev <dev.nammjuicy@gmail.com>
2022-08-04 20:08:41 +0900

    feat:모아존 시작

---
+++ frontend/src/api/Moazone.js
@@ -0,0 +1,35 @@
+import { apiAuth } from "./api"
+
+//모아글 전체 조회
+export const moaRead = ()  => {
+  const url = 'moazone';
+  return apiAuth.get(url);
+}
+
+// 모아글 생성
+export const moaCreate = (moa) => {
+  const url = 'moazone';
+  return apiAuth.post(url, moa);
+}
+// 모아글 수정
+export const moaUpdate = (moa, partyId) => {
+  const url = `moazone/${partyId}`;
+  return apiAuth.put(url, moa)
+}
+// 모아글 생성시 게임ID 검색
+export const moaGameSearch = () => {
+  const url = `moazone/games`;
+  return apiAuth.get(url);
+}
+
+// 모아글 상세
+export const moaDetail = (partyId) => {
+  const url = `moazone/${partyId}`;
+  return apiAuth.get(url)
+}
+
+// 모아글 검색
+export const moaSearch = () => {
+  const url = `moazone/search`;
+  return apiAuth.get(url);
+}
