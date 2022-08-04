2dd31c310bce00277adf80bb258c219ef23230cb
Jihyeon <jibstring@gmail.com>
2022-08-04 14:37:41 +0900

    fix : usertag entity 수정

--- BackEnd/src/main/java/com/ssafy/backend/db/entity/UserTag.java
+++ BackEnd/src/main/java/com/ssafy/backend/db/entity/UserTag.java
@@ -1,7 +1,9 @@
 package com.ssafy.backend.db.entity;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
@@ -9,21 +11,23 @@ import javax.persistence.*;
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserTag {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "usertag_id")
     private Long usertagId;
 
+    // 양방향 다대일
     @ManyToOne(fetch = FetchType.LAZY)
     @JsonBackReference
     @JoinColumn(name = "user_id")
     private User user;
 
+    // 단방향 다대일
     @ManyToOne(fetch = FetchType.LAZY)
     @JsonBackReference
     @JoinColumn(name = "utag_id")
     private UTagStorage uTagStorage;
-
-
 }
