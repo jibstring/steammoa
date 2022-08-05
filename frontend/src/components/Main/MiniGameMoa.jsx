85039b0b4d080d1c74b9e29f8d767f84048e5d7a
Yeon <theresjahn@gmail.com>
2022-08-05 16:43:00 +0900

    fix: 메인페이지 데이터 요청처리

--- frontend/src/components/Main/MiniGameMoa.jsx
+++ frontend/src/components/Main/MiniGameMoa.jsx
@@ -3,7 +3,11 @@ import MainGameComponent from './MainGameComponent'
 import {useState, useEffect} from 'react'
 
 const MiniGameMoa = (props) => {
+  const {bests, frees, today} = props
+  console.log(today)
+
   const [isMobile, setIsMobile] = useState(window.innerWidth < 640 ? true:false)
+
   
   //choose the screen size 
   const handleResize = () => {
@@ -27,14 +31,14 @@ const MiniGameMoa = (props) => {
         <div className='grid grid-cols-5 gap-3'>
           <div className='col-span-2 p-2'>
             <div className='w-full h-full flex flex-col justify-between items-center'>
-              <MainGameComponent/>
-              <MainGameComponent/>
-              <MainGameComponent/>
+              <MainGameComponent game={bests[Math.floor(Math.random() * 15)]} type="best"/>
+              <MainGameComponent game={frees[Math.floor(Math.random() * 15)]} type="free"/>
+              <MainGameComponent game={today[Math.floor(Math.random() * 14)]} type="today"/>
             </div>
     
           </div>
           <div className='col-span-3'>
-            <img src="https://cdn.akamai.steamstatic.com/steam/apps/9010/header.jpg?t=1573221747" alt="" className='rounded w-full drop-shadow-md'/>
+            {today.length ? <img src={today[14].gameImg} alt="" className='rounded w-full drop-shadow-md'/> : ""}
           </div>
         </div>
       
@@ -42,7 +46,14 @@ const MiniGameMoa = (props) => {
     )
   } else {
     return (
-      <></>
+      <>
+        <div className='w-full text-center font-Sans font-semibold text-lg tablet:text-xl laptop:text-2xl my-4 tablet:mt-6 miniGameMoa-neonText text-white'>GAME+</div>
+        <hr className='bg-createInput-gray mb-2'/>
+        <div className='w-full'>
+          {today.length ? <img src={today[14].gameImg} alt="" className='rounded w-full drop-shadow-md'/> : ""}
+        </div>
+      
+      </>
     )
   }
 }
