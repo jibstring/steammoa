9e080904967ca1c120009d420e4837922fe2110b
kimyeonjune <theresjahn@gmail.com>
2022-08-08 04:10:59 +0900

    feat: 리뷰 페이지네이션 완성

--- frontend/src/components/Game/GameReviewList.jsx
+++ frontend/src/components/Game/GameReviewList.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import GameReviewCreate from './GameReviewCreate'
 import GameReviewItem from './GameReviewItem'
+import MiniPagination from '../MiniPagination'
 import { getGameReviews } from '../../api/Review'
 import { useParams } from 'react-router-dom'
 import { range } from 'lodash'
@@ -9,78 +10,57 @@ import { range } from 'lodash'
 const GameReviewList = () => {
   const params = useParams()
   const gameId = params.game_id
-  const [reviewList, setReviewList] = useState([])
-  const [reviewPage, setReviewPage] = useState(1)
-  const [reviewCnt, setReviewCnt] = useState(0)
+  const [contentList, setContentList] = useState([])
+  const [page, setPage] = useState(1)
+  const [contentCnt, setContentCnt] = useState(0)
   const [totPage, setTotPage] = useState(0)
   const [viewablePages, setViewablePages] = useState([])
-  const reviewsPerPage = 1
-  const [showReviews, setShowReviews] = useState([])
+  const reviewsPerPage = 5
+  const [showContents, setShowContents] = useState([])
+  const [errMsg, setErrMsg] = useState('')
 
 
   useEffect(() => {
     getGameReviews(gameId)
       .then((res) => {
         console.log(res)
-        setReviewList(res.data.reviews)
+        setContentList(res.data.reviews)
+        setErrMsg('')
+
       })
       .catch((err)=>{
-        console.log(err)
+        setErrMsg('리뷰가 존재하지 않습니다. 첫번째 리뷰를 작성해주세요 :)')
       })
     }, [gameId])
 
   useEffect(()=>{
-    setReviewCnt(reviewList.length)
-    setTotPage(Math.ceil(reviewCnt/reviewsPerPage))
-    setViewablePages((reviewList.length ? [...range(1,Math.min(totPage,5)+1)]: []))
+    setContentCnt(contentList.length)
+    setTotPage(Math.ceil(contentCnt/reviewsPerPage))
+    setViewablePages((contentList.length ? [...range(1,Math.min(totPage,5)+1)]: []))
     
-    }, [reviewList, gameId, reviewCnt, totPage])
+    }, [contentList, gameId, contentCnt, totPage])
 
   useEffect(()=>{
-    const tmp = reviewList.splice((reviewPage-1)*reviewsPerPage+1,reviewPage*reviewsPerPage+1)
-    setShowReviews(tmp)
-  },[reviewPage, reviewList])
+    const tmp = contentList.slice((page-1)*reviewsPerPage,page*reviewsPerPage)
+    setShowContents(tmp)
+  },[page, contentList,])
 
-
-  const onClickItem = (e) =>{
-    const val = e.target.value
-    const view = (Math.ceil(val/5)-1) * 5
-    setReviewPage(val)
-    setViewablePages(range(view+1,totPage+1))
-  }
 
   return (
     <div className='p-4 w-full'>
       <GameReviewCreate></GameReviewCreate>
 
-      <div>
-        {showReviews.map((review, index)=>{
+      <div className='my-5'>
+        {(!contentList.length ? <div>{errMsg}</div>:<></>)}
+        {showContents.map((review, index)=>{
           return(
             <GameReviewItem review={review} key={index}/>
           )
         })}
       </div>
-
-      {/* pagination */}
-      <div id='pagination'
-        className="w-per35 flex justify-between">
-          {viewablePages.map((item, index)=>{
-            return(
-            <li
-            value={item}
-            key={index}
-            onClick={onClickItem}
-            className={`${
-              reviewPage===item ? "bg-main-300" : ""
-            } w-7 h-7  text-lg text-center list-none inline-block rounded-full hover:bg-main-100 hover:text-main-500`}
-            >
-              {item}
-            </li>             
-            )
-          })}
-
-
-      </div>
+      {(contentList.length ? 
+        <MiniPagination totPage={totPage} page={page} viewablePages={viewablePages} setPage={setPage} setViewablePages={setViewablePages}/> : <></> )}
+      
     </div>
   )
 }
