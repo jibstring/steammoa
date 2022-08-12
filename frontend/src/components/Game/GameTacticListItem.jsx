e6772fcb353e114da69ede6813ea9b30b5e7ad72
NamjuPark-dev <dev.nammjuicy@gmail.com>
2022-08-12 15:21:55 +0900

    feat:공략글 디테일 추가

--- frontend/src/components/Game/GameTacticListItem.jsx
+++ frontend/src/components/Game/GameTacticListItem.jsx
@@ -1,17 +1,28 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import { formatTime } from "../../util/FormatTime";
 
 function GameTacticListItem(props) {
   const { tacticId, tacticTitle, tacticContent, userId, gameId, userServiceId, createTime } =
     props.tactic;
 
-  const onClickTactic = () => {};
+  const navigate = useNavigate();
+
+  const onClickTactic = () => {
+    navigate(`/tactic/detail/${tacticId}`);
+  };
+
+  const onClickUser = (e) => {
+    e.stopPropagation(); 
+    navigate(`/profile/${userServiceId}`);
+  }
 
   return (
-    <div className="w-full h-32 border rounded drop-shadow-md my-1.5 bg-white p-3 flex flex-col justify-between">
+    <div
+      onClick={onClickTactic}
+      className="w-full h-32 border rounded drop-shadow-md my-1.5 bg-white p-3 flex flex-col justify-between">
       {/* 공략글 정보 */}
-      <div className="hover:cursor-pointer" onClick={onClickTactic}>
+      <div className="hover:cursor-pointer">
         {/* 공략글 제목 */}
         <div className=" w-fit font-sans font-semibold hover:font-bold mb-1 whitespace-nowrap overflow-hidden text-ellipsis hover:underline">
           {tacticTitle && tacticTitle}
@@ -24,11 +35,11 @@ function GameTacticListItem(props) {
       {/* 유저, 게시글 작성 시간 */}
       <div className="flex flex-row justify-end items-center">
         {userServiceId && (
-          <Link
-            to={`/profile/${userServiceId}`}
+          <span
+            onClick={onClickUser}
             className=" font-semibold text-moa-purple-light hover:cursor-pointer hover:text-moa-purple-dark">
             {userServiceId}
-          </Link>
+          </span>
         )}
         <span className="mx-2">{"."}</span>
         <span className="text-searchbar-gray algitn-text-center">
