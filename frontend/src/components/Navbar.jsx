770697b9e5db92524ca0c595b4fa7fda8ba2dc06
Yeon <theresjahn@gmail.com>
2022-08-15 21:57:12 +0900

    feat: 반응형 검색바 추가

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,12 +4,15 @@ import { useState } from "react";
 import NavbarLoginOptions from "./NavbarLoginOptions";
 import "../assets/style.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { faMagnifyingGlass, faXmark } from "@fortawesome/free-solid-svg-icons";
 import Swal from "sweetalert2";
 
 const Navbar = (props) => {
   const navigate = useNavigate();
   const [isMobile, setIsMobile] = useState(window.innerWidth < 640 ? true:false)
+  const [showSearchModal, setShowSearchModal] = useState(false)
+
+  const body = document.querySelector('body')
 
   
   //choose the screen size 
@@ -18,6 +21,7 @@ const Navbar = (props) => {
       setIsMobile(true)
     } else {
       setIsMobile(false)
+      setShowSearchModal(false)
     }
   }
   useEffect(()=>{
@@ -72,10 +76,22 @@ const Navbar = (props) => {
     }
   };
 
+  const onShowSearchModal = () => {
+    setShowSearchModal(true)
+    body.style.overflow = 'hidden';
+    body.style.position = 'fixed';
+  }
+
+  const onCloseSearchModal = () => {
+    setShowSearchModal(false)
+    body.style.removeProperty('overflow');
+    body.style.removeProperty('position');
+  }
+
   return (
     <div className="nav-container w-full nav-grad">
       {/* 내부 2칸 */}
-      <div className="w-per90 tablet:w-per75 m-auto">
+      <div className="w-per95 tablet:w-per75 m-auto">
         <div className="h-9 flex justify-between items-center">
           {/* 로고 */}
           <Link to="/" className="w-per15 min-w-90 max-w-144">
@@ -138,29 +154,51 @@ const Navbar = (props) => {
               </div>
             </div>
            :
-           <FontAwesomeIcon
-           icon={faMagnifyingGlass}
-           className="h-per35 hover:cursor-pointer text-white mr-2"
-          //  onClick={onShowSearchModal}
-           />
+           (showSearchModal ? 
+            <FontAwesomeIcon
+            icon={faXmark}
+            className="h-per35 hover:cursor-pointer font-bold text-white mr-2"
+            onClick={onCloseSearchModal}
+            />
+            :
+            <FontAwesomeIcon
+            icon={faMagnifyingGlass}
+            className="h-per35 hover:cursor-pointer text-white mr-2"
+            onClick={onShowSearchModal}
+            />
+           )
+           
            )}
 
         </div>
       </div>
 
             {/*follower modal */}
-        <div id="follower-modal" className={`${(true ? "bg-black bg-opacity-40":"hidden")} overflow-y-hidden overflow-x-hidden fixed top-[76px] right-0 left-0 z-50 w-full h-modal`}>
+        <div id="follower-modal" className={`${(showSearchModal ? "bg-black bg-opacity-[45%]":"hidden")} overflow-y-hidden overflow-x-hidden fixed top-[76px] right-0 left-0 z-50 w-full h-modal`}>
         <div className="relative w-full h-full md:h-auto mx-auto">
           {/* <!-- Modal content --> */}
-          <div className="relative top-0 right-0 left-0 bg-white shadow w-full">
+          <div className="relative top-0 right-0 left-0 nav-grad shadow w-full">
             {/* <!-- Modal header --> */}
-            <div className="flex justify-between items-center p-5 rounded-t">
-                <h3 className="text-base font-medium text-gray-800">
-                    <span className='text-xl font-semibold'>[]</span> 님의 Followers
-                </h3>
-                <button onClick="" className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center">
-                    <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
-                </button>
+            <div className="flex justify-between items-center p-3 rounded-t">
+              <div className="h-per70 w-full">
+                {/* 검색바 */}
+                <div className="flex items-center p-1 w-full text-gray-900 bg-gray-400 rounded-lg border border-gray-500 sm:text-xs focus:ring-slate-500 focus:border-slate-500 h-full">
+                  <input
+                    type="text"
+                    id="small-input"
+                    value={search}
+                    placeholder="@사용자 혹은 게임을 검색하세요"
+                    onChange={onChangeSearch}
+                    onKeyPress={onKeyPress}
+                    className="w-full h-5/6 text-gray-900 bg-gray-400 rounded-sm border-0 focus:border-none text-xs focus:border-slate-500border-transparent focus:border-transparent focus:ring-0"
+                  />
+                  <FontAwesomeIcon
+                    icon={faMagnifyingGlass}
+                    className="w-4 h-4 hover:cursor-pointer text-white mr-1"
+                    onClick={onSearch}
+                  />
+                </div>
+              </div>
             </div>
           </div>
         </div>
